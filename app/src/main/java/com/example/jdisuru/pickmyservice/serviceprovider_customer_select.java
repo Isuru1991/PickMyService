2c0fff39b3ef754a06927ab4a48cb0bf2f7c5814
christinleoni <leoni9446@gmail.com>
2018-11-12 23:52:05 +0530

    insert the select customer interface

---
+++ app/src/main/java/com/example/jdisuru/pickmyservice/serviceprovider_customer_select.java
@@ -0,0 +1,13 @@
+package com.example.jdisuru.pickmyservice;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class serviceprovider_customer_select extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_serviceprovider_customer_select);
+    }
+}
