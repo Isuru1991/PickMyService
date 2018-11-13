522d78d3dbc70fd3b57bfba3ee35973d280197f1
saradaindeevarie <sim.kahawandala@gmail.com>
2018-11-13 10:34:38 +0530

    select services interface

--- app/src/main/java/com/example/jdisuru/pickmyservice/select_services.java
+++ app/src/main/java/com/example/jdisuru/pickmyservice/select_services.java
@@ -9,5 +9,6 @@ public class select_services extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_select_services);
+
     }
 }
