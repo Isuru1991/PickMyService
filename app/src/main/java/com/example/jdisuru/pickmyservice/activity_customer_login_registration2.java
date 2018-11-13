4a9a238c304e27859aeaa38ec0f409b566e08d1b
isuru1991 <jdisuru@gmail.com>
2018-11-14 01:12:18 +0530

    new customer login part 2

---
+++ app/src/main/java/com/example/jdisuru/pickmyservice/activity_customer_login_registration2.java
@@ -0,0 +1,13 @@
+package com.example.jdisuru.pickmyservice;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class activity_customer_login_registration2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_customer_login_registration2);
+    }
+}
