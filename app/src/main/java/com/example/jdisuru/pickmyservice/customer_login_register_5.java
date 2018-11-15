30507dd21f12ca2d06512926e2b840b81f39fab7
isuru1991 <jdisuru@gmail.com>
2018-11-15 14:36:57 +0530

    login interface 5 but not complete

---
+++ app/src/main/java/com/example/jdisuru/pickmyservice/customer_login_register_5.java
@@ -0,0 +1,13 @@
+package com.example.jdisuru.pickmyservice;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class customer_login_register_5 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_customer_login_register_5);
+    }
+}
